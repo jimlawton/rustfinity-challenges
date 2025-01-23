7e6f9b170cebd58089dec9139779921459fa1ede
Jim Lawton <jim.lawton@gmail.com>
2025-01-23 14:04:25 +0000

    Macros.

---
+++ rust-fundamentals/73-macros/solution.rs
@@ -0,0 +1,35 @@
+#[macro_export]
+macro_rules! math_operations {
+    ($a:expr, $op:expr, $b:expr) => {{
+       match $op {
+            "+" => {
+                format!("{} + {} = {}", $a, $b, $a + $b)
+            }
+            "-" => {
+                format!("{} - {} = {}", $a, $b, $a - $b)
+            }
+            "*" => {
+                format!("{} * {} = {}", $a, $b, $a * $b)
+            }
+            "/" => {
+                if $b == 0 {
+                    panic!("Division by zero");
+                } else {
+                    format!("{} / {} = {}", $a, $b, $a / $b)
+                }
+            }
+            _ => {
+                panic!("Unsupported operator: {}", $op)
+            }
+        }
+    }};
+}
+
+// Example usage
+pub fn main() {
+    assert_eq!(math_operations!(4, "+", 2), "4 + 2 = 6");
+    assert_eq!(math_operations!(10, "-", 3), "10 - 3 = 7");
+    assert_eq!(math_operations!(6, "*", 4), "6 * 4 = 24");
+    assert_eq!(math_operations!(15, "/", 3), "15 / 3 = 5");
+}
+
